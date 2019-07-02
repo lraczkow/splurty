ef5fff3b7ab5f244bf53776aa400b91de72c32bf
Lauren Raczkowsk <lracz117@gmail.com>
2019-07-02 17:29:14 +0000

    meta name

--- app/controllers/quotes_controller.rb
+++ app/controllers/quotes_controller.rb
@@ -3,10 +3,6 @@ class QuotesController < ApplicationController
       @quote = Quote.order("RANDOM()").first
   end
 
-  def new
-    @quote = Quote.new
-  end
-
   def create
     @quote = Quote.create(quote_params)
     if @quote.invalid?
@@ -17,7 +13,7 @@ class QuotesController < ApplicationController
 
   def about
   end
-  
+
   private
 
   def quote_params
